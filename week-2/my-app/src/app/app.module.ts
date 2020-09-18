314bb9f00555d9445f3121b228b42cb6cf101a04
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-09-18 15:30:58 -0500

    Exercise 2.2

---
+++ week-2/my-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+//==================================
+// Title: app.module.ts
+// Author: Professor Krasso
+// Date: 17 September 2020
+// Modified by: Rochelle Markham
+// Description: application module
+//==================================
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
